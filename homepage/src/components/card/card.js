f864e3dd8e53f28327b207c9fef38c9fd14e3cae
masatomo57 <akagawa.code@gmail.com>
2025-04-26 14:09:01 +0900

    imageを削除

--- homepage/src/components/card/card.js
+++ homepage/src/components/card/card.js
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 /* https://zenn.dev/knaka0209/books/9e86ccb888758c/viewer/61072a */
@@ -6,7 +5,6 @@ import Link from 'next/link'
 export default function Card({ image, url, title, discription, tag=[] }) {
   return (
     <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2 rounded overflow-hidden shadow-lg">
-        <Image src={image} alt={title}/>
         <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">
           <Link href={url}>{title}</Link>
