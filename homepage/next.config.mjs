86ce6feba5e5de2a13d1fb5e54fc2007b373012c
masatomo57 <akagawa.code@gmail.com>
2025-05-04 09:40:12 +0900

    SSGに変更

--- homepage/next.config.mjs
+++ homepage/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Change to enable static site generation (SSG)
+    assetPrefix: './', // Set the asset prefix for static files
+};
 
 export default nextConfig;
