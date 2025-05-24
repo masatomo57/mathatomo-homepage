62fb6c41c8d15d0f804f8f766668c551a5610f9d
masatomo57 <akagawa.code@gmail.com>
2025-05-24 22:10:03 +0900

    snsアイコンの間を開けた

--- homepage/src/app/page.tsx
+++ homepage/src/app/page.tsx
@@ -6,20 +6,20 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-full bg-gray-100 text-amber-600">
       <h1 className="text-4xl font-bold">Welcome to まさとも&apos;s Website!</h1>
-      <div className="flex flex-row text-center mt-4">
-        <Link href="https://x.com/mathatomo57" className="underline hover:text-gray-800 flex items-center gap-2">
+      <div className="flex flex-row text-center mt-4 gap-4">
+        <Link href="https://x.com/mathatomo57" className="underline hover:text-gray-800 flex items-center">
           <FaXTwitter size={32} className="swing-hover" />
         </Link>
-        <Link href="https://www.instagram.com/mathatomo57?igsh=MTFvY2Y2Ym53N250aw%3D%3D&utm_source=qr" className="underline hover:text-gray-800 flex items-center gap-2">
+        <Link href="https://www.instagram.com/mathatomo57?igsh=MTFvY2Y2Ym53N250aw%3D%3D&utm_source=qr" className="underline hover:text-gray-800 flex items-center">
           <FaInstagram size={32} className="swing-hover" />
         </Link>
-        <Link href="https://www.facebook.com/share/19i7QWpB64/?mibextid=wwXIfr" className="underline hover:text-gray-800 flex items-center gap-2">
+        <Link href="https://www.facebook.com/share/19i7QWpB64/?mibextid=wwXIfr" className="underline hover:text-gray-800 flex items-center">
           <FaFacebook size={32} className="swing-hover" />
         </Link>
-        <Link href="https://www.linkedin.com/in/%E6%AD%A3%E6%9C%8B-%E8%B5%A4%E5%B7%9D-4485b2311/" className="underline hover:text-gray-800 flex items-center gap-2">
+        <Link href="https://www.linkedin.com/in/%E6%AD%A3%E6%9C%8B-%E8%B5%A4%E5%B7%9D-4485b2311/" className="underline hover:text-gray-800 flex items-center">
           <FaLinkedin size={32} className="swing-hover" />
         </Link>
-        <Link href="https://github.com/masatomo57" className="underline hover:text-gray-800 flex items-center gap-2">
+        <Link href="https://github.com/masatomo57" className="underline hover:text-gray-800 flex items-center">
           <FaGithub size={32} className="swing-hover" />
         </Link>
       </div>
